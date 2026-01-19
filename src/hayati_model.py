correct jac calculation

src/hayati_model.py
@@ -6,6 +6,9 @@ from math_routines import x_rot, y_rot, z_rot, arbitrary_axis_rot, trans
 from robotic_transformations import dh_trans, hayati_trans
 import multiprocessing as mp
 
+BIG_NUMBER = 10e300
+DEG = np.pi / 180
+
 class HayatiModel:
     def __init__(self, config):
         self.optimization_method = config['optimization_method']
@@ -41,14 +44,16 @@ class HayatiModel:
         self.general_samples_number = config["general_samples_number"]
         self.circle_samples_number = config["circle_samples_number"]
 
-        self.zero_tracker_position = config["zero_tracker_position"]
+        #self.zero_tracker_position = config["zero_tracker_position"]
+        self.zero_wire_angles = np.array(config["zero_wire_angles"]) * DEG
 
-        self.measurable_params_mask = np.array([0, 1, 2, 3, 4, 5], dtype='int')
+        self.measurable_params_mask = np.array([0, 1, 2, 3, 4, 5], dtype='int') 
 
         self.identifiability_mask = np.ones(36, dtype='int')
         self.koef = 0.001
         self.lm_koef = 0.01
         self.norm = 10
+        self.precision = config["precision"]
         self.prev_norm = 0
         self.num_point = 0
     
@@ -88,7 +93,20 @@ class HayatiModel:
             main_tf = main_tf @ tf
         return main_tf @ tool
     
-    def get_joint_coordinates_and_transition_matrix(self, angles: Union[np.ndarray, list], type: str) -> np.ndarray:
+    def change_nominal_dh(self, parametr: str, eps: float):
+        letter, num = parametr.split("_")
+        num = int(num)
+        if letter == 'a':
+            i = 0
+        elif letter == 'alpha':
+            i = 1
+        elif letter == 'd' or letter == 'beta':
+            i = 2
+        elif letter == 'theta':
+            i = 3  
+        self.nominal_dh[num - 1][i] += eps
+    
+    def get_all_transition_matrixes(self, angles: Union[np.ndarray, list], type: str) -> np.ndarray:
         if type == 'estimated':
             params = self.estimated_dh
             tool_params = self.estimated_tool_params
@@ -104,18 +122,15 @@ class HayatiModel:
         else:
             raise ValueError("type must be 'nominal', 'real' or 'estimated'")
         main_tf, tool = self.get_base_tool_tf(base_params, tool_params)
-        result = {"coords": [], "transition_matrix": []}
-        result["coords"].append(main_tf[:3, 3])
+        transition_matrixes = [main_tf]
 
         tfs = self.get_transforms(angles, params)
         for i, tf in enumerate(tfs):
             main_tf = main_tf @ tf
-            result["coords"].append(main_tf[:3, 3])
+            transition_matrixes.append(main_tf)
         
-        result["transition_matrix"] = main_tf @ tool
-        result["coords"].append([result["transition_matrix"][0][-1], result["transition_matrix"][1][-1], result["transition_matrix"][2][-1]])
-
-        return result
+        transition_matrixes.append(main_tf @ tool)
+        return transition_matrixes
     
     def satisfies_cartesian_limits(self, pose):
         position = pose[:3, 3]
@@ -124,3 +139,10 @@ class HayatiModel:
                 position[1] > self.cartesian_limits[1][0] and position[1] < self.cartesian_limits[1][1] and \
                 position[2] > self.cartesian_limits[2][0] and position[2] < self.cartesian_limits[2][1] and \
                 abs(z_angle) < self.max_z_angle)
+    
+    # Metrics and validation routines
+    def init_metrics(self):
+        self.cond = None
+        self.norm = None
+        self.max_x_error = self.max_y_error = self.max_z_error = self.max_dist_error = 0
+        self.min_x_error = self.min_y_error = self.min_z_error = self.min_dist_error = BIG_NUMBER

src/robot_visualization.py
@@ -8,11 +8,10 @@ import time
 from matplotlib import pyplot as plt
 import numpy as np
 from collections import deque
-import matplotlib
 import joystick 
 from ctypes import c_float
 import hayati_model
-from dataset_generation import read_dataset, FIELDNAMES_OPTIONS
+from dataset_generation_absolute import read_dataset, FIELDNAMES_OPTIONS
 
 class ShowRobot:
     def __init__(self):
@@ -46,11 +45,15 @@ class ShowRobot:
         # Initialize plots for all points
         self.current_points = []
         self.connection_lines = []
+        self.tool_axis = []
 
         # Add joint values text annotation (top-left)
         self.joint_text = self.fig.text(0.02, 0.8, '', transform=self.fig.transFigure, 
                                        fontsize=11, fontfamily='monospace', fontweight='bold',
                                        bbox=dict(boxstyle="round,pad=0.4", facecolor="lightblue", alpha=0.9))
+
+        self.axis_colors = ['red', 'green', 'blue']
+        self.axis_labels = ['X', 'Y', 'Z']
 
         # Trajectory line
         self.trajectory_line, = self.ax.plot([], [], [], "purple", alpha=0.7, linewidth=1.0)
@@ -68,14 +71,25 @@ class ShowRobot:
                                     linewidth=2.0,
                                     label=f'P{i+1}-P{i+2}' if i == 0 else "")
             self.connection_lines.append(conn_line)
-        
-        # Add legend
-        #self.ax.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+
+        # Draw base frame
+        original_axes = np.eye(3) * 0.2
+        last_trans_matrix = np.eye(4)
+        R = last_trans_matrix[:3, :3]
+        rotated_axes = R @ original_axes
+        for i in range(3):
+            self.ax.quiver(0, 0, 0,
+                    rotated_axes[0, i], rotated_axes[1, i], rotated_axes[2, i],
+                    color=self.axis_colors[i], label=self.axis_labels[i], linewidth=2,
+                    arrow_length_ratio=0.1)
         
         # Initial draw
         self.fig.canvas.draw()
         
-    def update_robot(self, points_coords, joint_values):
+    def update_robot(self, matrixes, joint_values):
+        points_coords = [m[:3, 3] for m in matrixes]
+        # for i in range(8):
+        #     points_coords.append(matrixes[i][:3, 3])
         
         # Update trajectory lines
         self.trajectory.append(points_coords[-1])
@@ -107,7 +121,24 @@ class ShowRobot:
             # Calculate segment length
             segment_length = np.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
             total_length += segment_length
-        
+  
+        # Draw each axis
+        axis_length = 0.1
+        original_axes = np.eye(3) * axis_length
+        R = matrixes[-1][:3, :3]
+        rotated_axes = R @ original_axes
+
+        for axis in self.tool_axis:
+            if axis in self.ax.collections:
+                axis.remove()
+        self.tool_axis.clear()
+        for i in range(3):
+            axis = self.ax.quiver(points_coords[-1][0], points_coords[-1][1], points_coords[-1][2],
+                            rotated_axes[0, i], rotated_axes[1, i], rotated_axes[2, i],
+                            color=self.axis_colors[i], label=self.axis_labels[i], linewidth=2,
+                            arrow_length_ratio=0.5)
+            self.tool_axis.append(axis)
+                    
         if hasattr(self, 'last_title_update'):
             if time.time() - self.last_title_update > 0.2:  # Update title every 200ms
                 self.ax.set_title(f'X: {points_coords[-1][0]:.3f}, Y: {points_coords[-1][1]:.3f}, Z: {points_coords[-1][2]:.3f}')
@@ -140,8 +171,8 @@ class ShowRobot:
         return plt.fignum_exists(self.fig.number)
 
 def visualize_pose(model: hayati_model.HayatiModel, plot: ShowRobot, angles_values: Union[np.ndarray, list], model_type):
-    coords_and_matrix = model.get_joint_coordinates_and_transition_matrix(angles_values, model_type)
-    plot.update_robot(coords_and_matrix["coords"], angles_values)
+    matrixes = model.get_all_transition_matrixes(angles_values, model_type)
+    plot.update_robot(matrixes, angles_values)
 
 def vizualize_forward_kinematics(model: hayati_model.HayatiModel, model_type="nominal"):
     running = mp.Value("i", 1)